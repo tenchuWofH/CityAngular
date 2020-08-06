f80417cdd3b58d258d9d08b327954404e8b7e6d8
Silvio Luiz Barbosa <silviob@geap.com.br>
2020-08-06 13:31:16 -0300

    add service

---
+++ src/app/services/city.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CityService {
+
+  constructor(private http: HttpClient) { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+  getData() {
+
+    return this.http.get('/api/City');  //https://localhost:44352/ webapi host url
+  }
+
+  postData(formData) {
+    return this.http.post('/api/City', formData);
+  }
+
+  putData(id, formData) {
+    return this.http.put('/api/City/' + id, formData);
+  }
+  deleteData(id) {
+    return this.http.delete('/api/City/' + id);
+  }
+}
